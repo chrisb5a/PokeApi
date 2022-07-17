22c493aac4c547323c4fb121f17b5cf593f37c07
CHRISTIAN BEYNIS <christianbeynis@CHRISTIANs-MacBook-Pro.local>
2022-07-17 16:00:52 -0400

    second Commit

--- FirstPokeApp copy/FirstPokeApp/Views/ViewController.swift
+++ FirstPokeApp copy/FirstPokeApp/Views/ViewController.swift
@@ -9,190 +9,180 @@ import UIKit
 
 class ViewController: UIViewController {
 
-//    lazy var manualDecodeButton: UIButton = {
-//        let button = UIButton(frame: .zero)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.setTitle("Manually Decode Data", for: .normal)
-//        button.setTitleColor(.white, for: .normal)
-//        button.backgroundColor = .systemBlue
-//        button.layer.cornerRadius = 15.0
-//        button.addTarget(self, action: #selector(self.manualDecodeButtonPressed), for: .touchUpInside)
-//        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
-//        return button
-//    }()
     var indexP = 0
-    var data = UIImage()
-//    var data = callNetwork2(val: indexP)
-    
-    
-    lazy var decodableDecodeButton: UIButton = {
-        let button = UIButton(frame: .zero)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Decodable Decode Data", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 15.0
-        button.addTarget(self, action: #selector(self.decodableButtonPressed), for: .touchUpInside)
-        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        return button
-    }()
-    
+
     lazy var progImageView2: UIImageView = {
         let imageView = UIImageView(frame: .zero)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleToFill
-        imageView.image = callNetwork2(val: indexP)
+        imageView.image = UIImage(named: "Mean_1")
         return imageView
         
     }()
-
     
+    lazy var Name: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = label.font.withSize(12)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        //label.text = "Pokemon_Name"
+        label.backgroundColor = .systemOrange
+        return label
+    }()
+    
+    lazy var Types: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = label.font.withSize(12)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        label.text = "Pokemon_type"
+        label.backgroundColor = .systemOrange
+        return label
+    }()
+    
+    
+    lazy var Abilities: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = label.font.withSize(12)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        label.text = "Pokemon_Abilities"
+//        label.backgroundColor = .systemOrange
+        return label
+    }()
+    
+    lazy var MovesList: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = label.font.withSize(12)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        label.text = "Pokemon_Moves"
+//        label.backgroundColor = .systemOrange
+        return label
+    }()
+
     let network = NetworkManager()
     
-    @objc
-    func decodableButtonPressed() {
-        let pokemon = network.getPokemonDecodable()
-        //self.network.getPokemonDecodable()
-        self.presentPokemonAlert(pokemon: pokemon, val: indexP)
-    }
-    
-//    @objc
-//    func manualDecodeButtonPressed() {
-//
-//        let pokemon = self.network.getPokemonManually()
-//        self.presentPokemonAlert(pokemon: pokemon)
-//    }
-    
-    func presentPokemonAlert(pokemon: Pokemon?, val: Int) {
-        guard let pokemon = pokemon else { return }
-        //var names = [String]()
-        //pokemon.forEach( value in names.append(value))
-        
-//        let names = "\(Array(pokemon.keys))"
-//        var name_arr = Array(pokemon.keys)
-        
-        
-//        let names1 = pokemon.pokemon.compactMap { $0.pokemon.name }.reduce("") { partialResult, name in
-//            return partialResult + name + "\n"
-//        }
-        
-        
-        
-        //print("\(String(describing: pokemon))")
-//        let names = pokemon.pokemon
-//        print("\(names)")
-//        print("ok")
-        
-//        let alert = UIAlertController(title: "name", message: "\(names)", preferredStyle: .alert)
-//        let action = UIAlertAction(title: "Fuck Off", style: .default, handler: nil)
-//        alert.addAction(action)
-//        DispatchQueue.main.async {
-//            self.present(alert, animated: true, completion: nil)}
-
-        let alert = UIAlertController(title: "names", message: String("\(pokemon[String(val+1)]![0])") , preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Sounds Good", style: .default, handler: nil)
-        alert.addAction(action)
-        DispatchQueue.main.async {
-            self.present(alert, animated: true, completion: nil)
-        }
-
-
-    }
-    
-    
-    private func callNetwork2(val: Int)->UIImage{
-        
-            
-            
-            let name_url_left = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
-            let name_url_right = ".png"
-            
-            
-            
-            
-            
-            let url = URL(string: name_url_left + String(val+1) + name_url_right)
-            
-            network.getData(url: url) { result in
-                switch result {
-                
-                case .success(let data):
-                    //self.currentPok += 1
-                    //self.PokInfo.append(1)
-                    DispatchQueue.main.async {
-                        self.data = UIImage(data: data) ?? UIImage(named: "Mean_1")!
-//                        self.table.reloadData()
-                        print("\(val)")
-                        
-                        
-                    }
-                    
-                    
-                
-                case .failure(let error):
-                    print(error)
-                    DispatchQueue.main.async {
-                        return
-                        
-                    }
-                }
-            
-            
-            
-            
-        }
-        
-        
-        return self.data
-        
-    }
-    
-    
-    //var indexP = 0
-    
-   
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemTeal
-        
         self.setUpUI()
-        
-
+        self.setLabels()
+        self.setImages()
         // Do any additional setup after loading the view.
     }
 
-    
-    
-    
     private func setUpUI() {
         
-        //self.progImageView2.image = callNetwork2(val:1)
+        self.view.addSubview(self.Name)
         self.view.addSubview(self.progImageView2)
-        //print("okookkkkk", "\(self.progImageView2.image)")
+        self.view.addSubview(self.Types)
+        self.view.addSubview(self.Abilities)
+        self.view.addSubview(self.MovesList)
         
+        self.Name.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        self.Name.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+        self.Name.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
         
         self.progImageView2.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
         self.progImageView2.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
-//
-        self.progImageView2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 44).isActive = true
-//
-        self.progImageView2.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
-       
-        self.progImageView2.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        self.progImageView2.widthAnchor.constraint(equalToConstant: 80).isActive = true
         
         
-        self.view.addSubview(self.decodableDecodeButton)
-        //self.view.addSubview(self.manualDecodeButton)
-        
-        self.decodableDecodeButton.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        self.decodableDecodeButton.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor, constant: -50).isActive = true
-        self.decodableDecodeButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
-        self.decodableDecodeButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+        self.Types.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        self.Types.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor, constant: 50).isActive = true
+        self.Types.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+        self.Types.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
         
         
     }
+    
+    func setLabels(){
+        
+        let pokemon = network.getPokemonDecodable()
+        guard let pokemon = pokemon else { return }
+        let name = String("\(pokemon[String(indexP+1)]![0])")
+        self.Name.text = "Name: " + name
+        let abilities = String("\(pokemon[String(indexP+1)]![1])")
+        self.Abilities.text = "Abilities: " + abilities
+        let type = String("\(pokemon[String(indexP+1)]![2])")
+        self.Types.text = "Types: " + type
+        let moves = String("\(pokemon[String(indexP+1)]![3])")
+        self.MovesList.text = "moves: " + moves
+    }
+    
+    func setImages (){
+        
+        self.network.fetchImageData(urlStr: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+String(indexP+1)+".png"){
+            result in switch result{
+                
+            case .success (let imageData):
+                DispatchQueue.main.async{
+                    self.progImageView2.image = UIImage(data: imageData)
+                }
+                
+            case .failure(let error):
+                print(error)
+
+            
+            
+            
+            }
+            
+        }
+        
+    }
+
+        
+  
+//        self.Name.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+//        self.Name.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+//
+//        self.Name.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+//
+//
+//
+//        self.view.addSubview(self.progImageView2)
+//
+//
+//        self.progImageView2.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 48).isActive = true
+//        self.progImageView2.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -48).isActive = true
+//
+//        self.progImageView2.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+////
+//  //      self.progImageView2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 20).isActive = true
+////
+//        //self.progImageView2.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+//
+//        self.progImageView2.heightAnchor.constraint(equalToConstant: 80).isActive = true
+//        self.progImageView2.widthAnchor.constraint(equalToConstant: 20).isActive = true
+//
+//        self.view.addSubview(self.Abilities)
+//        self.view.addSubview(self.MovesList)
+//
+//
+//        self.MovesList.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        self.MovesList.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor, constant: -50).isActive = true
+//        self.MovesList.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+//        self.MovesList.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+//
+//        self.view.addSubview(self.decodableDecodeButton)
+//        //self.view.addSubview(self.manualDecodeButton)
+//
+//        self.decodableDecodeButton.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        self.decodableDecodeButton.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor, constant: -50).isActive = true
+//        self.decodableDecodeButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+//        self.decodableDecodeButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+        
+        
+    
     
     
     }
